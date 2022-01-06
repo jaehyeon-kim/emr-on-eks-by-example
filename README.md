6d4902bcf429308fb2bfc1a237636d1c21435861
Jaehyeon Kim <dottami@gmail.com>
2022-01-07 10:12:53 +1100

    add more set up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # emr-on-eks-by-example
+
+```bash
+## initial setup, creates eks/hudi config files and downloads hudi library
+./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export S3_BUCKET_NAME=${CLUSTER_NAME}-${AWS_REGION}
 
 mkdir -p config && mkdir -p manifests
 
-## cluster and node group config files
+echo "create cluster and node group config files..."
 cat << EOF > ./manifests/cluster.yaml
 ---
 apiVersion: eksctl.io/v1alpha5
@@ -46,7 +46,7 @@ managedNodeGroups:
   spot: true
 EOF
 
-## pod templates
+echo "create pod templates..."
 cat << EOF > ./config/driver_pod_template.yml
 apiVersion: v1
 kind: Pod
@@ -63,7 +63,55 @@ spec:
     eks.amazonaws.com/capacityType: SPOT
 EOF
 
-## Hudi config files and utility bundle library
+echo "create example python script and download sample data..."
+cat << EOF > ./config/health_violations.py
+import argparse
+
+from pyspark.sql import SparkSession
+
+def calculate_red_violations(data_source, output_uri):
+    """
+    Processes sample food establishment inspection data and queries the data to find the top 10 establishments
+    with the most Red violations from 2006 to 2020.
+
+    :param data_source: The URI of your food establishment data CSV, such as 's3://DOC-EXAMPLE-BUCKET/food-establishment-data.csv'.
+    :param output_uri: The URI where output is written, such as 's3://DOC-EXAMPLE-BUCKET/restaurant_violation_results'.
+    """
+    with SparkSession.builder.appName("Calculate Red Health Violations").getOrCreate() as spark:
+        # Load the restaurant violation CSV data
+        if data_source is not None:
+            restaurants_df = spark.read.option("header", "true").csv(data_source)
+
+        # Create an in-memory DataFrame to query
+        restaurants_df.createOrReplaceTempView("restaurant_violations")
+
+        # Create a DataFrame of the top 10 restaurants with the most Red violations
+        top_red_violation_restaurants = spark.sql("""SELECT name, count(*) AS total_red_violations 
+          FROM restaurant_violations 
+          WHERE violation_type = 'RED' 
+          GROUP BY name 
+          ORDER BY total_red_violations DESC LIMIT 10""")
+
+        # Write the results to the specified output URI
+        top_red_violation_restaurants.write.option("header", "true").mode("overwrite").csv(output_uri)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--data_source', help="The URI for you CSV restaurant data, like an S3 bucket location.")
+    parser.add_argument(
+        '--output_uri', help="The URI where output is saved, like an S3 bucket location.")
+    args = parser.parse_args()
+
+    calculate_red_violations(args.data_source, args.output_uri)
+EOF
+
+curl -s -o ./config/food_establishment_data.zip \
+  https://docs.aws.amazon.com/emr/latest/ManagementGuide/samples/food_establishment_data.zip \
+  && unzip ./config/food_establishment_data.zip -d ./config \
+  && rm ./config/food_establishment_data.zip
+
+echo "create Hudi config files and download hudi utility lib..."
 cat << EOF > ./config/cdc_events_s3.properties
 ## base properties
 hoodie.upsert.shuffle.parallelism=2
